8316331aa279d84ae30c7a2551661603d4848e5a
Arvind Goyal <goyalarvind1104@gmail.com>
2026-09-10 10:27:19 +0530

    routing

--- server/app.js
+++ server/app.js
@@ -1,39 +1,31 @@
 import express from "express";
 import cors from "cors";
-// import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import analysisRoutes from "./routes/analysisRoutes.js";
 import editorRoutes from "./routes/editorRoutes.js";
-
-import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
 import historyRoutes from "./routes/historyRoutes.js";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
 import profileRoutes from "./routes/profileRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
-
-
-// dotenv.config();
-// console.log(process.env.GEMINI_API_KEY);
-
 const app = express();
 
-// app.use(cors());
+app.use(
+  cors({
+    origin: "https://ai-mentor-neon-ten.vercel.app",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.json({
     message: "AI DSA Mentor Backend is running",
   });
 });
-
-import cors from 'cors';
-app.use(cors({
-  origin: 'https://ai-mentor-neon-ten.vercel.app',
-  credentials: true
-}));
-app.use(express.json());
-
-app.use(cookieParser());
 
 app.use("/api/analyze", analysisRoutes);
 app.use("/api/editor", editorRoutes);
