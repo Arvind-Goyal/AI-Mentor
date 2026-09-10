1a887ef0b9d25bbb5f421967baeed38432672127
Arvind Goyal <goyalarvind1104@gmail.com>
2026-09-10 10:03:37 +0530

    test

--- server/app.js
+++ server/app.js
@@ -20,6 +20,11 @@ import userRoutes from "./routes/userRoutes.js";
 const app = express();
 
 // app.use(cors());
+app.get("/", (req, res) => {
+  res.json({
+    message: "AI DSA Mentor Backend is running",
+  });
+});
 
 app.use(
     cors({
