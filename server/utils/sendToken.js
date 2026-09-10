c70582055e41d3120d41dd1792dbe4e2b0899945
Arvind Goyal <goyalarvind1104@gmail.com>
2026-09-10 13:14:50 +0530

    token_issue

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -6,7 +6,7 @@ const sendToken = (user, statusCode, res, message) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    sameSite: "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
