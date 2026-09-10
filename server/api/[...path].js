0fdbd035c5f53d599e309a551a0ba8594cb585c1
Arvind Goyal <goyalarvind1104@gmail.com>
2026-09-10 11:06:17 +0530

    deploment 4

--- server/api/[...path].js
+++ server/api/[...path].js
@@ -2,6 +2,13 @@ import "dotenv/config";
 import connectDB from "../config/db.js";
 import app from "../app.js";
 
+app.get("/api/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "Catch-all Vercel function is working",
+  });
+});
+
 await connectDB();
 
 export default app;
